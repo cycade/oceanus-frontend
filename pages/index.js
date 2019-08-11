85cbe8c435b4770378d0ef055d30cb628a3c108a
Inaho <inahocyc@gmail.com>
2019-08-11 16:59:42 +1000

    apply purecss module

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function Home() {
         <div>
             <Head>
                 <title>Oceanus page</title>
-                <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.1/build/pure-min.css" integrity="sha384-oAOxQR6DkCoMliIh8yFnu25d7Eq/PHS21PClpwjOTeU2jRSq11vu66rf90/cZr47" crossorigin="anonymous"></link>
+                <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.1/build/pure-min.css" integrity="sha384-oAOxQR6DkCoMliIh8yFnu25d7Eq/PHS21PClpwjOTeU2jRSq11vu66rf90/cZr47" crossOrigin="anonymous"></link>
             </Head>
         <div>Blue Planet</div>
         <a className="pure-button" href="#">A Pure Button</a>
